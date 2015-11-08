127727243f58c4f4d28bc9034189773bed37a0f5
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-11-08 22:35:26 +0100

    Fix double nav because of href="#"

--- js/components/menu.js
+++ js/components/menu.js
@@ -5,12 +5,25 @@ import component from '../component';
 import smartComponent from '../omniscient-redux';
 import Actions from '../actions/actions';
 
+const preventDefaultThen = (fn) => (e) => {
+  e.preventDefault();
+  return fn();
+};
+
 const Menu = smartComponent(
   (_) => ({}),
   component('Menu', ({ dispatch }) =>
     div({},
       ul({},
-        li({}, a({ href: "#", onClick: () => dispatch(Actions.updatePath("/")) }, "Index route")),
-        li({}, a({ href: "#", onClick: () => dispatch(Actions.updatePath("/another")) }, "Another route"))))));
+         li({}, a({
+           href: "",
+           onClick: preventDefaultThen(
+             () => dispatch(Actions.updatePath("/"))) },
+                  "Index route")),
+         li({}, a({
+           href: "",
+           onClick: preventDefaultThen(
+             () => dispatch(Actions.updatePath("/another"))) },
+                  "Another route"))))));
 
 export default Menu;
